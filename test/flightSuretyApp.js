7b3b5fc6bff5bad422900d10da3dd50be7bae01d
Tiger Wang <tigerwang@outlook.com>
2022-05-07 19:20:04 -0400

    wip

--- test/flightSuretyApp.js
+++ test/flightSuretyApp.js
@@ -60,6 +60,49 @@ contract('Flight Surety Tests', async (accounts) => {
     assert.equal(result, true, "Airline should be registered");
   });
 
+  it('(airline) cannot register an Airline without multipart consensus after 4 airlines already registered', async () => {
+    let registeredAirlineCount = await config.flightSuretyApp.getRegisteredAirlineCount.call();
+
+    assert(registeredAirlineCount < 4, 'Too many airlines registered');
+
+    // fund
+    await config.flightSuretyApp.sendTransaction({
+      value: web3.utils.toWei('10', 'ether'),
+      from: config.firstAirline
+    });
+
+    // ARRANGE
+    [3, 4].forEach(async (i) => {
+      let newAirline = accounts[i];
+
+      // ACT
+      try {
+        await config.flightSuretyApp.registerAirline(newAirline, { from: config.firstAirline });
+      }
+      catch (e) {
+
+      }
+      let result1 = await config.flightSuretyApp.isAirline.call(newAirline);
+
+      // ASSERT
+      assert.equal(result1, true, "Airline should be registered");
+    });
+
+    let additionalAirline = accounts[5];
+
+    // ACT
+    try {
+      await config.flightSuretyApp.registerAirline(additionalAirline, { from: config.firstAirline });
+    }
+    catch (e) {
+
+    }
+    let result2 = await config.flightSuretyApp.isAirline.call(additionalAirline);
+
+    // ASSERT
+    assert.equal(result2, false, "Airline should be registered");
+  });
+
 
   /****************************************************************************************/
   /* Operations and Settings                                                              */

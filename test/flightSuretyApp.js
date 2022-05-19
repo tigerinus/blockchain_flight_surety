07eec63bc33af6de185ec808dbdf17cff6c5f702
Tiger Wang (王豫) <tigerwang@outlook.com>
2022-05-18 22:46:13 -0400

    (#6)

--- test/flightSuretyApp.js
+++ test/flightSuretyApp.js
@@ -1,9 +1,35 @@
 
 var Test = require('../config/testConfig.js');
 
+const STATUS_CODE_UNKNOWN = 0;
+const STATUS_CODE_ON_TIME = 10;
+const STATUS_CODE_LATE_AIRLINE = 20;
+const STATUS_CODE_LATE_WEATHER = 30;
+const STATUS_CODE_LATE_TECHNICAL = 40;
+const STATUS_CODE_LATE_OTHER = 50;
+
+const STATUS_CODES = [
+  STATUS_CODE_UNKNOWN,
+  STATUS_CODE_ON_TIME,
+  STATUS_CODE_LATE_AIRLINE,
+  STATUS_CODE_LATE_WEATHER,
+  STATUS_CODE_LATE_TECHNICAL,
+  STATUS_CODE_LATE_OTHER
+];
+
 contract('Flight Surety Tests', async (accounts) => {
 
+  /**
+   * account  0 is the owner of the contract
+   * accounts  1 - 9  are airlines
+   * accounts 10 - 19 are passengers
+   * accounts 20 - 49 are oracles
+   */
+
   var config;
+
+  const timestamp = Math.floor(Date.now() / 1000);
+
   before('setup contract', async () => {
     config = await Test.Config(accounts);
   });
@@ -229,4 +255,121 @@ contract('Flight Surety Tests', async (accounts) => {
     assert.equal(accessDenied, false, "Access not restricted to Contract Owner");
 
   });
+
+  it(`8 - (airline) can register flight if the airline is registered`, async () => {
+    // ARRANGE
+    let flight = 'ND1309';
+    let airline = accounts[1];
+
+    let result = await config.flightSuretyApp.isAirline.call(airline);
+    assert.equal(result, true, "airline should be registered");
+
+    // ACT
+    let succeed = true;
+
+    try {
+      await config.flightSuretyApp.registerFlight(flight, timestamp, { from: airline });
+    }
+    catch (e) {
+      succeed = false;
+    }
+
+    // ASSERT
+    assert.equal(succeed, true, "flight registration should succeed");
+  });
+
+  it(`9 - (airline) cannot register flight if the airline is not registered`, async () => {
+    // ARRANGE
+    let flight = 'ND1309';
+    let airline7 = accounts[7];
+
+    let result = await config.flightSuretyApp.isAirline.call(airline7);
+    assert.equal(result, false, "airline should not be registered");
+
+    // ACT
+    let succeed = true;
+
+    try {
+      await config.flightSuretyApp.registerFlight(flight, timestamp, { from: airline7 });
+    }
+    catch (e) {
+      succeed = false;
+    }
+
+    // ASSERT
+    assert.equal(succeed, false, "flight registration should fail");
+  });
+
+  it(`10 - (passenger) can buy insurance for a flight for less than 1 ether`, async () => {
+    let passenger = accounts[10];
+
+    let airline1 = accounts[1];
+    let flight = 'ND1309';
+
+    let succeed = true;
+
+    try {
+      await config.flightSuretyApp.buy(airline1, flight, timestamp, { from: passenger, value: web3.utils.toWei('0.5', 'ether') });
+    }
+    catch (e) {
+      succeed = false;
+    }
+
+    assert.equal(succeed, true, "passenger should be able to buy insurance for less than 1 ether");
+  });
+
+  it(`11 - (passenger) cannot buy insurance for a flight for more than 1 ether`, async () => {
+    let passenger = accounts[10];
+
+    let airline1 = accounts[1];
+    let flight = 'ND1309';
+
+    let succeed = true;
+
+    try {
+      await config.flightSuretyApp.buy(airline1, flight, timestamp, { from: passenger, value: web3.utils.toWei('1.5', 'ether') });
+    }
+    catch (e) {
+      succeed = false;
+    }
+
+    assert.equal(succeed, false, "passenger should be able to buy insurance for less than 1 ether");
+  });
+
+  it(`12 - (passenger) does not receive credit if flight is delayed due to weather`, async () => {
+    let passenger = accounts[10];
+
+    let airline1 = accounts[1];
+    let flight = 'ND1309';
+
+    await config.flightSuretyApp.processFlightStatus(airline1, flight, timestamp, STATUS_CODE_LATE_WEATHER, { from: config.owner });
+
+    let creditBalance = await config.flightSuretyApp.getCreditBalance.call({ from: passenger });
+
+    assert.equal(creditBalance.toNumber(), 0, "credit balance should be 0 for the passenger");
+
+    let result = await config.flightSuretyApp.withdraw.call({ from: passenger });
+
+    assert.equal(result, false, "credit withdraw should fail");
+  });
+
+  it(`12 - (passenger) receives credit if flight is delayed due to airline`, async () => {
+    let passenger = accounts[10];
+
+    let airline1 = accounts[1];
+    let flight = 'ND1309';
+
+    await config.flightSuretyApp.processFlightStatus(airline1, flight, timestamp, STATUS_CODE_LATE_AIRLINE, { from: config.owner });
+
+    let creditBalance = await config.flightSuretyApp.getCreditBalance.call({ from: passenger });
+
+    assert.equal("750000000000000000", creditBalance.toString(), "credit balance should be 0.75 ether which is 1.5x of 0.5 ether insured in test 10");
+
+    await config.flightSuretyApp.withdraw.call({ from: passenger });
+
+    let result = await config.flightSuretyApp.withdraw.call({ from: passenger });
+
+    assert.equal(result, true, "credit withdraw should succeed");
+  });
+
 });
